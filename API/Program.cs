fc9efb82324b00b775e49a6573a387755d8f6f8c
mtabassum <mtabassum015@gmail.com>
2025-05-08 21:12:13 -0500

    Added Material UI, HTTPS Support and Axios

---
+++ API/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+}
+);
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedData(context);
+
+}catch(Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration.");
+}
+
+
+
+app.Run();
---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class AppDbContext(DbContextOptions options): DbContext(options)
+    {
+        public required DbSet<Activity> Activities { get; set; }
+    }
+}
